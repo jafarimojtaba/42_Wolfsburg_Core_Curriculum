13e67da220dac5266c9882d3e955a05bfd154195
mjafari <mojtabaj.official@gmail.com>
2021-08-07 03:50:43 +0200

    calloc

--- Libft/dev/ft_calloc.c
+++ Libft/dev/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mjafari <mjafari@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/24 15:28:17 by mjafari           #+#    #+#             */
-/*   Updated: 2021/05/31 15:31:34 by mjafari          ###   ########.fr       */
+/*   Updated: 2021/08/06 11:31:51 by mjafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
+	ptr = NULL;
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (0);
