7e9a310ea90b3996a06a077e9edac612ec0b06a5
Mojtaba Jafari <mojtabaj.official@gmail.com>
2021-12-25 13:54:20 +0100

    minitalk v01, problem in long msgs: solution: client should call server by one bit then server call client by one delivery approve until the end of string

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,11 +6,72 @@
 /*   By: mjafari <mjafari@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 22:05:53 by mjafari           #+#    #+#             */
-/*   Updated: 2021/12/20 22:27:57 by mjafari          ###   ########.fr       */
+/*   Updated: 2021/12/25 13:37:34 by mjafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char *argv[])
+#include "minitalk.h"
+
+void usr_handler(int delivery, siginfo_t *siginfo, void *context)
 {
+	static int delivered = 0;
+
+	(void)siginfo;
+	(void)context;
+	if (delivery == SIGUSR1)
+		delivered++;
+	if (delivery == SIGUSR2)
+	{
+		write(1, "delivered: ", 11);
+		ft_putnbr_fd(delivered, 1);
+		write(1, "\n", 1);
+		exit(0);
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	struct sigaction sa;
+	static int sent;
+	char c;
+	int i;
+	int j;
+
+	j = 0;
+	if (argc != 3)
+		return (0);
+	while (argv[2][j])
+	{
+		c = argv[2][j];
+		i = 7;
+		while (i > -1)
+		{
+			if (c & 1)
+				kill(ft_atoi(argv[1]), SIGUSR1);
+			else
+				kill(ft_atoi(argv[1]), SIGUSR2);
+			c = c >> 1;
+			i--;
+			usleep(100);
+		}
+		sent++;
+		j++;
+	}
+	i = 8;
+	while (i > 0)
+	{
+		kill(ft_atoi(argv[1]), SIGUSR2);
+		i--;
+		usleep(100);
+	}
+	sent++;
+	write(1, "sent: ", 6);
+	write(1, ft_itoa(sent), sent / 10 + 1);
+	write(1, "\n", 1);
+	sa.sa_sigaction = usr_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
 	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,67 @@
 /*   By: mjafari <mjafari@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 22:06:08 by mjafari           #+#    #+#             */
-/*   Updated: 2021/12/20 22:06:09 by mjafari          ###   ########.fr       */
+/*   Updated: 2021/12/25 13:37:29 by mjafari          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+void usr_handler(int bit, siginfo_t *siginfo, void *context)
+{
+	static int recived = 0;
+
+	static int arr[8];
+	static int i;
+	int c;
+
+	if (i == 0)
+		i = 8;
+	if (bit == SIGUSR1)
+		bit = 1;
+	else
+		bit = 0;
+	arr[--i] = bit;
+	if (i == 0)
+	{
+		c = 0;
+		while (i < 8)
+		{
+			c = c << 1;
+			c = c | arr[i];
+			i++;
+		}
+		write(1, &c, 1);
+		recived++;
+		ft_bzero(arr, 8);
+		if (c == 0)
+		{
+			while (recived > 0)
+			{
+				usleep(400);
+				kill(siginfo->si_pid, SIGUSR1);
+				recived--;
+			}
+			usleep(800);
+			kill(siginfo->si_pid, SIGUSR2);
+			write(1, "\n", 1);
+		}
+	}
+	(void)context;
+}
+
+int main(void)
+{
+	struct sigaction sa;
+
+	write(1, "Server_PID: ", 12);
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
+	sa.sa_sigaction = usr_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
+	while (1)
+		pause();
+	return (0);
+}
